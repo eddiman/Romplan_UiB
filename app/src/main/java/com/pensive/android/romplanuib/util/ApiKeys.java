6b4048051c15860450c84ecdd358771ef264c676
Fredrik Heimsæter <freheims@gmail.com>
2017-05-21 20:09:56 +0200

    Added ApiKeys-class.

---
+++ app/src/main/java/com/pensive/android/romplanuib/util/ApiKeys.java
@@ -0,0 +1,13 @@
+package com.pensive.android.romplanuib.util;
+
+/**
+ * @author Edvard Pires Bjørgen
+ */
+
+public class ApiKeys {
+
+    public String getUibApiKey(){
+        return "Add your API-key here";
+
+    }
+}
