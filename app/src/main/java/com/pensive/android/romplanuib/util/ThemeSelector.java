46d96d87a0610db1557ef0c5110b9a33b1b46154
Fredrik Heimsæter <fredrik@heimseter.com>
2019-02-05 20:32:41 +0100

    Add themes for new universities

--- app/src/main/java/com/pensive/android/romplanuib/util/ThemeSelector.java
+++ app/src/main/java/com/pensive/android/romplanuib/util/ThemeSelector.java
@@ -19,6 +19,12 @@ public class ThemeSelector {
 
             case "uio":
                 return context.getTheme().getResources().getIdentifier("DialogGreenTheme", "style", context.getPackageName());
+            case "ntnu":
+                return context.getTheme().getResources().getIdentifier("DialogNTNUTheme", "style", context.getPackageName());
+            case "uit":
+                return context.getTheme().getResources().getIdentifier("DialogPurpleTheme", "style", context.getPackageName());
+            case "oslomet":
+                return context.getTheme().getResources().getIdentifier("DialogRedTheme", "style", context.getPackageName());
         }
         return 0;
     }
@@ -29,6 +35,12 @@ public class ThemeSelector {
                 return context.getTheme().getResources().getIdentifier("BlueTheme", "style", context.getPackageName());
             case "uio":
                 return context.getTheme().getResources().getIdentifier("GreenTheme", "style", context.getPackageName());
+            case "ntnu":
+                return context.getTheme().getResources().getIdentifier("NTNUTheme", "style", context.getPackageName());
+            case "uit":
+                return context.getTheme().getResources().getIdentifier("PurpleTheme", "style", context.getPackageName());
+            case "oslomet":
+                return context.getTheme().getResources().getIdentifier("RedTheme", "style", context.getPackageName());
         }
         return 0;
     }
